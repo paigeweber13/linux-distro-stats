ed97ec5aab73a33a8d9b412d621463cea1f58674
Brian Weber <brian.weber1337@gmail.com>
2018-07-01 21:36:25 -0400

    descripes a distribution

---
+++ disro.py
@@ -0,0 +1,8 @@
+class distro:
+    """ describes a linux distribution """
+    def __init__(self, name = None,
+                       popularity = None,
+                       reviews = None):
+        self.name = name
+        self.popularity = popularity
+        self.reviews = reviews
