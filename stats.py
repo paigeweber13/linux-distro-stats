56af99fee9437810c8c273e40ed67d62fe5ae465
Brian Weber <brian.weber1337@gmail.com>
2018-07-01 21:36:02 -0400

    functions only do one thing now instead of doing the same thing repeatedly to a list or map of thigns

--- stats.py
+++ stats.py
@@ -7,18 +7,25 @@ __author__ = "Brian Weber"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
+# doesn't work right now... not sure why...?
+# import distro
+
+# third party imports
 from bs4 import BeautifulSoup
 
 import urllib.request
 
-# constants
+### CONSTANTS ###
 reviewPageBaseUrl = "https://distrowatch.com/dwres.php?resource=ratings&distro="
-
 # temp, will be replaced by text from a file
 distrosToCheck = ["slackware"]
+linuxDistros = []
 
 # functions
-def getReviewsPagesHtml(distroNames):
+def is_review_cell(tag):
+    return tag.name == 'td' and tag.has_attr('width') and tag['width'] == '70%'
+
+# def getReviewsPagesHtml(distroNames):
     """ gets html for distrowatch.com review pages for a list of linux distros
 
     arguments:
@@ -29,32 +36,45 @@ def getReviewsPagesHtml(distroNames):
                   httpResponse objects
     """
     
-    reviewPageHtml = {}
-    for distro in distroNames:
-        reviewPageHtml[distro] = urllib.request.urlopen(
-            reviewPageBaseUrl + distro)
+    # reviewPageHtml = {}
+    # for distro in distroNames:
+    #     reviewPageHtml[distro] = urllib.request.urlopen(
+    #         reviewPageBaseUrl + distro)
     # print(reviewPageHtml)
     # print(reviewPageHtml["slackware"].read())
+    # return reviewPageHtml
+
+def getReviewsPageHtml(distroName):
+    reviewPageHtml = urllib.request.urlopen(reviewPageBaseUrl + distroName)
     return reviewPageHtml
 
-def extractReviewText(htmlArray):
-    """ given a list of httpresponse objects, extracts distrowatch review text, if any.
+def extractReviewText(reviewPageHtml):
+    soup = BeautifulSoup(reviewPageHtml, "html.parser")
+    reviewCells = soup.find_all(is_review_cell)
+    reviewsText = ''
+    for cell in reviewCells:
+        cell.form.decompose()
+        reviewsText += cell.text
+        reviewsText += ' '
+    reviewsText = reviewsText.replace('\n', ' ')
+    reviewsText = reviewsText.replace('\r', ' ')
+    reviewsText = reviewsText.replace('.', ' ')
+    reviewsText = reviewsText.replace('(', ' ')
+    reviewsText = reviewsText.replace(')', ' ')
+    reviewsText = reviewsText.replace(',', ' ')
+    reviewsText = reviewsText.replace('!', ' ')
+    reviewsText = reviewsText.replace(',', ' ')
 
-    arguments:
-    htmlArray -- a list of httpResponse objects retrieved from distrowatch.com
-                 review pages.
+    # html = htmlDictionary['slackware'].read()
+    # html = htmlDictionary['slackware']
+    # soup = BeautifulSoup(html, "html.parser")
+    # reviewText = soup.select('td[width="70%"]')
 
-    return value: a dictionary where distro names are the keys and review text
-                  are the values
-    """
-    html = htmlArray['slackware'].read()
-    soup = BeautifulSoup(html, "html.parser")
-    soup.select('td[width="70%"]')
-
+    return reviewsText
 
 # main
 def main():
-    return extractReviewText(getReviewsPagesHtml(distrosToCheck))
+    return extractReviewText(getReviewsPageHtml(distrosToCheck[0]))
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
