db3056de716cfdf21d56a173b61ecd8a21185131
Brian Weber <brian.weber1337@gmail.com>
2018-07-01 21:38:03 -0400

    initial commit: two routes, one for a basic html page and one for a page where I test python things

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+import stats
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/distro-stats")
+def get_distro_stats():
+    return stats.main()
+
+# @app.route("/js/<script_name>")
+# def resolve(script_name):
+#     return 'wip'
+
+# @app.route("/hello")
+# def hello():
+#     return "Hello world!"
