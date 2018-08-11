205fb5dcf2d935b85fd587b563b9a480634b4674
Brian Weber <brian.weber1337@gmail.com>
2018-08-10 20:48:36 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='linuxstats',
+      version='0.1',
+      description="""website that displays aggregate statistics of
+      distributions gathered from distrowatch.com""",
+      url='https://github.com/brianweber13/linux-distro-stats',
+      author='Brian Weber',
+      author_email='brian.weber1337@gmail.com',
+      license='GPLv3')
