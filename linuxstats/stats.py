cae48087f0fc2657ce5b0a590140798371090565
Brian Weber <brian.weber1337@gmail.com>
2018-08-10 22:54:24 -0400

    cleanup, added docstrings

--- linuxstats/stats.py
+++ linuxstats/stats.py
@@ -7,38 +7,37 @@ from .distro import Distro
 
 ### CONSTANTS ###
 REVIEW_PAGE_BASE_URL = "https://distrowatch.com/dwres.php?resource=ratings&distro="
-# temp, will be replaced by text from a file
-#distrosToCheck = ["slackware"]
+# temp, will be replaced by titles sourced from a file
 linux_distros = [Distro('slackware')]
 
 # functions
 def is_review_cell(tag):
+    """ checks if a tag is a review cell with certain attributes that are
+    unique to review cells on distrowatch.com
+
+    Args:
+        tag (BeautifulSoup.tag)
+    Returns:
+        bool:
+    """
     return tag.name == 'td' and tag.has_attr('width') and tag['width'] == '70%'
 
-# def getReviewsPagesHtml(distroNames):
+def get_reviews_page_html(distro_name):
     """ gets html for distrowatch.com review pages for a list of linux distros
 
-    arguments:
-    distroNames -- a list of distribution names, with the spelling and
-                   capitalization used in distrowatch.com links
+    Args:
+        distro_name (str): a list distribution name, with the spelling and
+                           capitalization used in distrowatch.com links
 
-    return value: a dictionary where keys are distro names and values are
-                  httpResponse objects
+    Returns:
+        str: the html of the page in plaintext
     """
-
-    # reviewPageHtml = {}
-    # for distro in distroNames:
-    #     reviewPageHtml[distro] = urllib.request.urlopen(
-    #         REVIEW_PAGE_BASE_URL + distro)
-    # print(reviewPageHtml)
-    # print(reviewPageHtml["slackware"].read())
-    # return reviewPageHtml
-
-def get_reviews_page_html(distro_name):
     review_page_html = urllib.request.urlopen(REVIEW_PAGE_BASE_URL + distro_name)
     return review_page_html
 
 def extract_review_text(review_page_html):
+    """ gets review text cells from plaintext html and combines all the review
+    text into one string """
     soup = BeautifulSoup(review_page_html, "html.parser")
     review_cells = soup.find_all(is_review_cell)
     reviews_text = ''
@@ -55,21 +54,4 @@ def extract_review_text(review_page_html):
     reviews_text = reviews_text.replace('?', ' ')
     #get rid of extra whitespace
     reviews_text = ' '.join(reviews_text.split())
-
-    # html = htmlDictionary['slackware'].read()
-    # html = htmlDictionary['slackware']
-    # soup = BeautifulSoup(html, "html.parser")
-    # reviewText = soup.select('td[width="70%"]')
-
     return reviews_text
-
-# main
-#import os
-def main():
-    #print(os.environ['PYTHONPATH'].split(os.pathsep))
-    return "<body>hello from python</body>"
-    #return extract_review_text(get_reviews_page_html(distros_to_check[0]))
-
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
