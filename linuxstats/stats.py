5e258a18934c5b3d589ef75b287ff6a81c193703
Brian Weber <brian.weber1337@gmail.com>
2018-08-19 14:48:03 -0400

    functions not return soup and accept soup as argument instead of html text

--- linuxstats/stats.py
+++ linuxstats/stats.py
@@ -8,9 +8,10 @@ from .distro import Distro
 ### CONSTANTS ###
 REVIEW_PAGE_BASE_URL = "https://distrowatch.com/dwres.php?resource=ratings&distro="
 # temp, will be replaced by titles sourced from a file
-linux_distros = [Distro('slackware')]
+LINUX_DISTROS = [Distro('slackware')]
 
-# functions
+### functions ###
+# Reviews
 def is_review_cell(tag):
     """ checks if a tag is a review cell with certain attributes that are
     unique to review cells on distrowatch.com
@@ -22,7 +23,7 @@ def is_review_cell(tag):
     """
     return tag.name == 'td' and tag.has_attr('width') and tag['width'] == '70%'
 
-def get_reviews_page_html(distro_name):
+def get_reviews_page_html_soup(distro_name):
     """ gets html for distrowatch.com review pages for a list of linux distros
 
     Args:
@@ -30,16 +31,17 @@ def get_reviews_page_html(distro_name):
                            capitalization used in distrowatch.com links
 
     Returns:
-        str: the html of the page in plaintext
+        soup: a 'soup' of the html from the reviews page that is parseable by
+              BeautifulSoup4
     """
     review_page_html = urllib.request.urlopen(REVIEW_PAGE_BASE_URL + distro_name)
-    return review_page_html
+    soup = BeautifulSoup(review_page_html, 'html.parser')
+    return soup
 
-def extract_review_text(review_page_html):
+def extract_review_text(review_page_soup):
     """ gets review text cells from plaintext html and combines all the review
     text into one string """
-    soup = BeautifulSoup(review_page_html, "html.parser")
-    review_cells = soup.find_all(is_review_cell)
+    review_cells = review_page_soup.find_all(is_review_cell)
     reviews_text = ''
     for cell in review_cells:
         cell.form.decompose()
