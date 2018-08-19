bcb4a4a38abcb75b1937d22c8c4339f9a32051dc
Brian Weber <brian.weber1337@gmail.com>
2018-08-19 14:51:10 -0400

    added other properties that will be useful

--- linuxstats/distro.py
+++ linuxstats/distro.py
@@ -1,8 +1,9 @@
 class Distro:
     """ describes a linux distribution """
-    def __init__(self, name=None,
-                 popularity=None,
-                 reviews=None):
+    def __init__(self, name, main_page_soup = None, review_page_soup = None,
+                 popularity=None, reviews=None):
         self.name = name
+        self.main_page_soup = main_page_soup
+        self.review_page_soup = review_page_soup
         self.popularity = popularity
         self.reviews = reviews
