edcbdb41f7817889f28d152090e609e75a9deab7
Brian Weber <brian.weber1337@gmail.com>
2018-09-23 11:07:47 -0400

    now extracts popularity from main page and ratings from individual reviews

--- linuxstats/stats.py
+++ linuxstats/stats.py
@@ -1,5 +1,9 @@
-# third party imports
+# python imports
+from datetime import date
+import re
 import urllib.request
+
+# third party imports
 from bs4 import BeautifulSoup
 
 # from this package
@@ -7,11 +11,12 @@ from .distro import Distro
 
 ### CONSTANTS ###
 REVIEW_PAGE_BASE_URL = "https://distrowatch.com/dwres.php?resource=ratings&distro="
+MAIN_PAGE_BASE_URL = "https://distrowatch.com/table.php?distribution="
 # temp, will be replaced by titles sourced from a file
 LINUX_DISTROS = [Distro('slackware')]
 
 ### functions ###
-# Reviews
+# get soups
 def get_reviews_page_html_soup(distro_name):
     """ gets html for distrowatch.com review pages for a list of linux distros
 
@@ -27,6 +32,13 @@ def get_reviews_page_html_soup(distro_name):
     soup = BeautifulSoup(review_page_html, 'html.parser')
     return soup
 
+def get_main_page_html_soup(distro_name):
+    """ gets html of main distrowatch page """
+    main_page_html = urllib.request.urlopen(MAIN_PAGE_BASE_URL + distro_name)
+    soup = BeautifulSoup(main_page_html, 'html.parser')
+    return soup
+
+# Reviews
 def is_review_cell(tag):
     """ checks if a tag is a review cell with certain attributes that are
     unique to review cells on distrowatch.com
@@ -57,3 +69,51 @@ def extract_review_text(review_page_soup):
     #get rid of extra whitespace
     reviews_text = ' '.join(reviews_text.split())
     return reviews_text
+
+# ratings
+def is_rating_cell(tag):
+    """ checks if tag holds a rating """
+    return tag.name == 'td' and tag.has_attr('width') and tag['width'] == '30%'
+
+def extract_ratings(review_page_soup):
+    rating_cells = review_page_soup.find_all(is_rating_cell)
+    ratings = {}
+    rating_regex = re.compile(r"""
+            Rating:(?P<rating>\d+)
+            Date:(?P<year>\d+)-(?P<month>\d+)-(?P<day>\d+)
+            Votes""", re.VERBOSE)
+    for cell in rating_cells:
+        text = ''.join(cell.text.split())
+        result = rating_regex.search(text)
+        rating_date = date(int(result.group('year')),
+                           int(result.group('month')),
+                           int(result.group('day')))
+        print(rating_date)
+        ratings[rating_date] = int(result.group('rating'))
+    return ratings
+
+# popularity metric
+def extract_popularity(main_page_soup):
+    info_cell = main_page_soup.find("td", class_="TablesTitle")
+    text = ''.join(info_cell.text.split())
+    popularity_regex = re.compile(r"""
+        Popularity\(hitsperday\).*
+        12months:(?P<twelve_month_score>\d+).*
+        6months:(?P<six_month_score>\d+).*
+        3months:(?P<three_month_score>\d+).*
+        4weeks:(?P<four_week_score>\d+).*
+        1week:(?P<one_week_score>\d+)
+        """, re.VERBOSE)
+    print('text:')
+    print(text)
+    print('regex:')
+    print(popularity_regex)
+    result = popularity_regex.search(text)
+    print('result')
+    print(result)
+    popularity = {'1w':int(result.group('one_week_score')),
+                  '4w':int(result.group('four_week_score')),
+                  '3m':int(result.group('three_month_score')),
+                  '6m':int(result.group('six_month_score')),
+                  '12m':int(result.group('twelve_month_score')), }
+    return popularity
--- tests/test_stats.py
+++ tests/test_stats.py
@@ -4,6 +4,7 @@
 does NOT test getting html from the internet"""
 
 import unittest
+from datetime import date
 from bs4 import BeautifulSoup
 
 import context
@@ -28,38 +29,77 @@ class StatsTestCase(unittest.TestCase):
                     </title>
                 </head>
             <body>
-                <td width="70%" style="border:0" valign="top">
-                    This is my review text. Lorem ipsum dolor sit amet,\r
-                    consectetur adipiscing elit! Vestibulum lacinia tortor sed
-                    erat suscipit pharetra? Aliquam (tincidunt) elementum\r
-                    dapibus. Quisque ut, vulputate, et nibh. Duis ultricies a
-                    augue eu dictum...()?!,'.
-                    <br><br><br>
-                    <form>
-                    </form>
-                </td>
-                <td width="70%" style="border:0" valign="top">
-                    This is a different review. Duis at tempor lectus. Sed at
-                    aliquam lorem, vitae suscipit ex.  Praesent at lorem est.
-                    Ut sagittis enim sollicitudin aliquet ullamcorper. In quis
-                    velit nec metus tempus pellentesque.  Phasellus eget mauris
-                    porta, vulputate ex nec, venenatis lorem.  Morbi vel
-                    fringilla enim. Nulla at ante id dolor gravida porttitor ut
-                    sodales odio. 
-                    <br><br><br>
-                    <form>
-                    </form>
-                </td>
+                <tr>
+                    <td width="30%" style="border:0" valign="top">
+                        <b>Version:</b> 14.2<br><b>Rating:</b>
+                        10<br><b>Date:</b> 2018-08-10<br><b>Votes:</b>
+                        17<br><br><br>
+                    </td>
+                    <td width="70%" style="border:0" valign="top">
+                        This is my review text. Lorem ipsum dolor sit amet,\r
+                        consectetur adipiscing elit! Vestibulum lacinia tortor
+                        sed erat suscipit pharetra? Aliquam (tincidunt)\r
+                        elementum dapibus. Quisque ut, vulputate, et nibh.
+                        Duis ultricies a augue eu dictum...()?!,'.
+                        <br><br><br>
+                        <form>
+                        </form>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="30%" style="border:0" valign="top">
+                        <b>Version:</b> current<br><b>Rating:</b>
+                        10<br><b>Date:</b> 2018-08-01<br><b>Votes:</b>
+                        5<br><br><br>
+                    </td>
+                    <td width="70%" style="border:0" valign="top">
+                        This is a different review. Duis at tempor lectus. Sed at
+                        aliquam lorem, vitae suscipit ex.  Praesent at lorem est.
+                        Ut sagittis enim sollicitudin aliquet ullamcorper. In quis
+                        velit nec metus tempus pellentesque.  Phasellus eget mauris
+                        porta, vulputate ex nec, venenatis lorem.  Morbi vel
+                        fringilla enim. Nulla at ante id dolor gravida porttitor ut
+                        sodales odio. 
+                        <br><br><br>
+                        <form>
+                        </form>
+                    </td>
+                </tr>
             </body>
             </html>
             """
-        self.soup = BeautifulSoup(self.test_review_page_html, 'html.parser')
+        self.review_page_soup = BeautifulSoup(self.test_review_page_html, 'html.parser')
         #self.test_distro = linuxstats.distro.Distro(TEST_DISTRIBUTION_NAME)
+        self.test_main_page_html = """
+            <td class="TablesTitle">
+                <a href="images/fakenix.png">
+                    <img src="images/fakenix-small.png" border="0"
+                    title="Fakenix Unix Linux" vspace="6" hspace="6"
+                    align="right">
+                </a>
+                <b>
+                    <a href="dwres.php?resource=popularity">Popularity (hits per
+                    day)</a>:
+                </b>
+                12 months: <b>29</b> (313), 6 months: <b>29</b> (305), 3
+                months: <b>21</b> (307), 4 weeks: <b>25</b> (353), 1 week:
+                <b>36</b> (307)<br><br><b><a
+                href="ratings/resource/link">Average visitor rating</a></b>:
+                <b>9.4</b>/10 from <b>139</b> <a
+                href="ratings/link/for/fakenix">review(s)</a>.<br><br>
+            </td>
+            """
+        self.main_page_soup = BeautifulSoup(self.test_main_page_html,
+            'html.parser')
 
     def test_is_review_cell(self):
         """ tests if our function to find cells that contain review text works
+        self.review_page_soup.find_all('td') finds all td tags and returns an array of
+        them. Using index one, we check to see if the second td tag is a review
+        cell, which it is. This can be verified by checking the html above
         """
-        self.assertTrue(linuxstats.stats.is_review_cell(self.soup.td))
+        cell = self.review_page_soup.find_all('td')[1]
+        self.assertTrue(linuxstats.stats.is_review_cell(cell))
 
     def test_extract_review_text(self):
         """ tests if we can get multiple reviews and properly eliminate
@@ -78,12 +118,36 @@ class StatsTestCase(unittest.TestCase):
                         odio """
         expected_text = ' '.join(expected_text.split())
         actual_text = linuxstats.stats.extract_review_text(
-            self.soup)
+            self.review_page_soup)
         print('text we got:')
         print(actual_text)
         print('text we expected:')
         print(expected_text)
         self.assertEqual(actual_text, expected_text)
+
+    def test_is_rating_cell(self):
+        """ tests if our function to find cells that contain ratings works
+        """
+        cell = self.review_page_soup.find_all('td')[0]
+        self.assertTrue(linuxstats.stats.is_rating_cell(cell))
+
+    def test_extract_ratings(self):
+        expected_dict = {date(2018, 8, 10): 10, date(2018, 8, 1): 10}
+        actual_dict = linuxstats.stats.extract_ratings(self.review_page_soup)
+        print('expected dictionary:')
+        print(expected_dict)
+        print('actual dictionary:')
+        print(actual_dict)
+        self.assertEqual(actual_dict, expected_dict)
+
+    def test_extract_popularity(self):
+        expected_dict = {'1w':36, '4w':25, '3m':21, '6m':29, '12m':29}
+        actual_dict = linuxstats.stats.extract_popularity(self.main_page_soup)
+        print('expected dictionary:')
+        print(expected_dict)
+        print('actual dictionary:')
+        print(actual_dict)
+        self.assertEqual(actual_dict, expected_dict)
 
 if __name__ == '__main__':
     unittest.main()
