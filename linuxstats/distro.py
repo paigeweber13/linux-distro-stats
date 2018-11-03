156c63957aebb48bccb75ff4bcf2e63ca67f7d8d
Brian Weber <brian.weber1337@gmail.com>
2018-11-02 22:35:31 -0400

    restructured object. Metrics now contained in one object

--- linuxstats/distro.py
+++ linuxstats/distro.py
@@ -1,19 +1,22 @@
-"""
-    popularity (dict): 12 month, 6 month, 3 month, 4 week, and 1 week
-                       popularity metric. Eventually I will fetch the one week
-                       popularity once a week and store it so I can create my
-                       own popularity metrics over any length of time
-                       maps 12m, 6m, 3m, 4w, and 1w to popularity score
-    ratings (dict): maps date to rating
-    reviews (str): plaintext content of reviews
-"""
 class Distro:
     """ describes a linux distribution """
-    def __init__(self, name, main_page_soup=None, review_page_soup=None,
-                 popularity=None, ratings=None, reviews=None):
+    """
+    metrics (dict) contains:
+            popularity (dict): popularity metric. Eventually I will fetch the
+                               one week popularity once a week and store it so
+                               I can create my own popularity metrics over any
+                               length of time. Currently maps 12m, 6m, 3m, 4w,
+                               and 1w to a popularity score
+            ratings (dict): maps date to rating
+            easy (dict): maps date of review to how much that review added to
+                         this score
+            reliable (dict): maps date to metric contribution
+            fast (dict): maps date to contribution
+            customizable (dict): maps date to contribution
+    """
+
+    def __init__(self, name,
+                 main_page_soup=None, review_page_soup=None,
+                 popularity=None, ratings=None, reviews=None, metrics=None):
         self.name = name
-        self.main_page_soup = main_page_soup
-        self.review_page_soup = review_page_soup
-        self.popularity = popularity
-        self.ratings = ratings
-        self.reviews = reviews
+        self.metrics = metrics
